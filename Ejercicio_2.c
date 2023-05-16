fe8026b262cbb2a5b9d9aadb05b4afbb74af6290
Roberto-Sainz <124394461+Roberto-Sainz@users.noreply.github.com>
2023-05-15 19:30:09 -0700

    Create Ejercicio_2.c

---
+++ Ejercicio_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void despliegaAhorcado(char* nombreArchivoFigura);
+int main(){
+despliegaAhorcado("figura_5.txt");
+  return 0;
+}
+void despliegaAhorcado(char* nombreArchivoFigura) {
+    FILE* archivo = fopen(nombreArchivoFigura, "r");
+    if (archivo == NULL) {
+        printf("No se puede abrir el archivo %s\n", nombreArchivoFigura);
+        return;
+    }
+    char linea[100];
+    while (fgets(linea, sizeof(linea), archivo)) {
+        printf("%s", linea);
+    }
+    fclose(archivo);
+}
